7d721684f26684ee9da7e2c90ca6cf9a22a88491
José Armando García Sancio <jsancio@gmail.com>
2014-12-15 21:14:49 -0800

    Update gitconfig and remove unused options

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/dircolors-solarized/dircolors.ansi-dark ~/.dircolors
-ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/ctags ~/.ctags
