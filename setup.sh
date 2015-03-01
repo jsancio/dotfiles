06867a933af7decd129c5a59a8c7478c778a9483
José Armando García Sancio <jsancio@gmail.com>
2015-02-28 22:54:55 -0800

    Change update_bundles to python

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/gitignore ~/.gitignore
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/dircolors-solarized/dircolors.ansi-dark ~/.dircolors
-ln -s ~/.dotfiles/ctags ~/.ctags
+#!/bin/sh
+
+BASEDIR=$(python -c "import os; print(os.path.realpath('$(dirname "$0")'))")
+
+ln -s $BASEDIR/vimrc ~/.vimrc
+ln -s $BASEDIR/vim ~/.vim
+ln -s $BASEDIR/tmux.conf ~/.tmux.conf
+ln -s $BASEDIR/gitignore ~/.gitignore
+ln -s $BASEDIR/gitconfig ~/.gitconfig
+# TODO: Fix this being a git submodule
+# ln -s $BASEDIR/dircolors-solarized/dircolors.ansi-dark ~/.dircolors
+ln -s $BASEDIR/ctags ~/.ctags
